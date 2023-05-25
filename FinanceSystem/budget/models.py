0c13dedfb7e2b120cc6616a157247e609e559bfe
macwojDev <maciekwojdyna1@gmail.com>
2023-05-25 18:02:00 +0200

    poprawa modeli(nadal bez migracji), utworzenie zarysu szablonu i widoku dla budget

--- FinanceSystem/budget/models.py
+++ FinanceSystem/budget/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Expenses(models.Model):
+    name = models.CharField(max_length=40)
+#   dwa miejsca po przecinku
+    cost = models.DecimalField(max_digits=12, decimal_places=2)
+    description = models.TextField(blank=True, null = True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    buyer = models.ForeignKey(User, related_name='expenses', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ("-created_at")
+        verbose_name_plural= 'Expenses'
+    # elementy tabeli zwracają swoją nazwe 
+
+class Budget(models.Model):
+    wealth = models.DecimalField(max_digits=12, decimal_places=2)
+    daily_limit = models.FloatField(max_length=9)
+
+    user = models.ForeignKey(User, related_name='budget', on_delete=models.CASCADE, unique=True)
+    expenses = models.ForeignKey(Expenses, related_name='budget', on_delete=models.CASCADE)
+    
+    class Meta:
+        verbose_name_plural = 'Budgets'
+    
+    def __str__(self):
+        return f"{self.user.username}'s Budget"
--- FinanceSystem/budget/views.py
+++ FinanceSystem/budget/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Expenses, Budget
 
-# Create your views here.
+def budget(request):
+    budget = Budget.objects.filter(user=request.user)
+    expenses = Expenses.objects.filter(created_by=budget.user.username)
+    context = {
+        'expenses':expenses,
+        'budget':budget
+    }
+    return render(request, context)
+
+
