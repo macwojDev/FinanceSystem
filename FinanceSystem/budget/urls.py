156cac01d8a00efeccdf7b3caadd3858756f4724
macwojDev <maciekwojdyna1@gmail.com>
2023-06-02 19:40:32 +0200

    formularze expense i income

--- FinanceSystem/budget/urls.py
+++ FinanceSystem/budget/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('budget_status/', views.budget_status, name='budget_status'),
     path('budget_creation/', views.budget_creation, name='budget_creation'),
-    path('new_expense/', views.new_expense, name='new_expense')
-    # path('expense_detail/<int:pk>', views.expense_detail, name='expense_detail')
+    path('new_expense/', views.new_expense, name='new_expense'),
+    path('new_income/', views.new_income, name='new_income'),
+    path('expense_detail/<int:pk>', views.expense_detail, name='expense_detail'),
+    path('income_detail/<int:pk>', views.income_detail, name='income_detail')
 ]
--- FinanceSystem/budget/views.py
+++ FinanceSystem/budget/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Expense, Budget, Income
-from .forms import BudgetCreationForm, ExpenseCreationForm
+from .forms import BudgetCreationForm, ExpenseCreationForm, IncomeCreationForm
 
+# widok budżetu, centralnej części aplikacji
 def budget_status(request):
     budget_exists = Budget.objects.filter(user=request.user).exists()
     if budget_exists:
@@ -14,10 +15,11 @@ def budget_status(request):
             'incomes':incomes
         }   
         return render(request, 'budget/budget_status.html' ,context)
+#   przekierowanie do formularza utworzenia budżetu, w momencie gdy użytkownik go nie posiada
     else:
         return redirect('budget_creation')
     
-
+# utworzenie budżetu
 def budget_creation(request):
     form = BudgetCreationForm()
     if request.method == 'POST':
@@ -33,24 +35,56 @@ def budget_creation(request):
         'form' : form
     })
 
+# Widoki dodawania zmian w budżecie 
 def new_expense(request):
-    budget = Budget.objects.filter(user=request.user)
+    budget = Budget.objects.get(user=request.user)
     form = ExpenseCreationForm()
     if request.method == 'POST':
         form = ExpenseCreationForm(request.POST)
         if form.is_valid():
             expense = form.save(commit=False)
             expense.owner = request.user
-            budget.expenses.add(expense)
             expense.save()
+            budget.wealth = float(budget.wealth)
+            budget.wealth -= float(expense.cost)
+            budget.expenses.add(expense)
             budget.save()
+            return redirect('budget_status')
         else:
             form = ExpenseCreationForm()
-    return render(request, 'budget/new_expense.html')
+    return render(request, 'budget/forms.html',{
+        'form':form
+    })
+
+def new_income(request):
+    budget = Budget.objects.get(user=request.user)
+    form = IncomeCreationForm()
+    if request.method == 'POST':
+        form = IncomeCreationForm(request.POST)
+        if form.is_valid():
+            income = form.save(commit=False)
+            income.money_owner = request.user
+            income.save()
+            budget.wealth = float(budget.wealth)
+            budget.wealth += float(income.income)
+            budget.incomes.add(income)
+            budget.save()
+            return redirect('budget_status')
+        else:
+            form = IncomeCreationForm()
+    return render(request, 'budget/forms.html', {
+        'form':form
+    })
+
 
 def expense_detail(request, pk):
     expense = get_object_or_404(Expense, pk=pk)
     return render(request, 'budget/expense_detail.html', {
         'expense':expense
+    })
+def income_detail(request, pk):
+    income = get_object_or_404(Expense, pk=pk)
+    return render(request, 'budget/expense_detail.html', {
+        'expense':income
     })
 
